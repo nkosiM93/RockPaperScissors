0a087a5bd7d3c2aca4113d38dc3cc3a9634a7f18
Mathiya <mathiyathokozanin@gmail.com>
2026-05-27 07:29:22 +0200

    Fixed prompt error 2.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function getComputerChoice() {
 
 // Get the user's choice
 function getUserChoice() {
-    return prompt("Rock, paper, scissors, GO!!!. Enter choice: ").toLowerCase();
+    return prompt("Rock, paper, scissors, GO!!!. \nEnter choice: ").toLowerCase();
 }
 
 //Game play
