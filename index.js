029cdbb85b34997c29e9978549f19effc90917de
Mathiya <mathiyathokozanin@gmail.com>
2026-05-27 06:36:26 +0200

    Added gamePlay().
     modified: index.js
    
     Added gamePlay(). This function brings together the entire logic
     of the game.
    
     Added 5 calls to the gamePLay() function to simulate
     5 rounds of the game.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// Coding the Computer's choice of the game
-console.log("Rock, paper, scissors ?"); // Game call
-
 // Randomize the computer's choice between the three choices
-let compChoice = getComputerChoice(); // computer choice
-let userChoice = getUserChoice(); //User choice variable
+let compChoice;
+let userChoice;
 let userScore = 0;
 let compScore = 0;
+let round = 1;
 
 // Get the computer's random choice
 function getComputerChoice() {
 
     // Choices
     switch(Math.trunc(Math.random() * 10) % 3){ 
-        /*Randomize the computer's choice each time this function 
-        is called */
+        /* 
+        Randomize the computer's choice each time this function 
+        is called 
+        */
         case 0:
             return "rock";
         case 1:
@@ -30,9 +30,6 @@ function getUserChoice() {
 
 //Game play
 function playRound(userChoice, compCh) {
-
-
-
     // handle ties immediately
     if (userChoice === compCh) {
         return "It's a tie!";
@@ -70,6 +67,26 @@ function playRound(userChoice, compCh) {
 
     return "Invalid choice, refresh the browser";
 }
+
+// Game-play function (main funtion)
+function gamePlay() {
+    console.log(`Round ${round++}!`); // Round number
+    console.log("Rock, paper, scissors, GO!!!\n"); // Game call
+    compChoice = getComputerChoice(); // computer choice
+    userChoice = getUserChoice(); //User choice
+    console.log(`Player 1 choice: ${compChoice}`); // Computer's choice
+    console.log(`Your choice: ${userChoice}`); // User's choice
+    console.log(playRound(userChoice, compChoice)); // Play the game
+    console.log(`Your score: ${userScore}`);
+    console.log(`Computer score: ${compScore}`);
+    console.log("\n-----------------------------\n");
+}
+
+gamePlay(); // Call the game-play function
+gamePlay();
+gamePlay();
+gamePlay();
+gamePlay();
 
 /*console.log(`Player 1 choice: ${compChoice}`); // Computer's choice
 console.log(`Your choice: ${userChoice}`); // User's choice
